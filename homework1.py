82438da69fa96360d608be24a47a547c6f98042a
Jeans617 <JeanGonzalez3.14@berkeley.edu>
2025-02-04 12:47:46 -0800

    adding homework 1

---
+++ homework1.py
@@ -0,0 +1,15 @@
+# 1 pwd: prints current working directory, you'd use this to see what directory you are currently in. pwd -P displays the physical current directory, not a link
+# cd: changes current directory to a different one, you'd use this to access files in a different directory. 
+# ls: prints everything in the current directory, used to see what files are in this directory. ls -a shows all files including hidden ones. 
+# mkdir: makes a new directory, used when you want to shift to a new project in a new directory. mkdir -p makes a parent directory if one does not exist.
+# cp: copies a file, used when you want to copy a file for later use or use elsewhere. cp -r copies a directory
+# mv: moves files and directories from one directory to another, used when you want to change the location of a file. mv -b makes a backup of the files that would overriden
+
+# 2 Git is something you can use in your terminal to save versions of your code while working. GitHub is used to share your code and download code from the internet. Git bash is a command terminal that allows windows users to use git commands.
+# The terminal is the windo used to enter commands, while command line is where you enter the actual command.
+# Windows powershell is the native terminal for windows but it uses a different language than git bash which is used in Mac and Linux
+# Anaconda is a program that contains various packages including conda which is a package manager. Python is a codig language that is used in programs like anaconda.
+# A Jupyter notebook is an interface used in anaconda that allows for visualizations and equations using python. A jupyter Lab is like a Jupyter notebook but more advanced and flexible user interface. 
+# VS Code is a common text editor that we use to write and edit python code. 
+
+
