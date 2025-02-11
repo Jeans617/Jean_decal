5a744592b60f9608eb7360c79148f16040589015
Jeans617 <JeanGonzalez3.14@berkeley.edu>
2025-02-11 13:34:41 -0800

    adding hw2

---
+++ hw2.py
@@ -0,0 +1,14 @@
+# 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+# cd ~/desktop
+# 2) What does ~/ mean?
+#It's a shortcut for the home directory for the user. 
+# 3) What's the difference between and abolute and a relative path in your terminal?
+#The absolute path used the full directory structure while the relative path is dependent on what your current directory is.
+# 4) What command would bring you back to your homework directory?
+# cd ~/Desktop/classes/python_decal/homework
+# 5) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+# I was reallt confused why I wasn't able to push stuff to github, but then i realized my default brainch wast "master" instead of "main" so I just had to change that. 
+# 6) How does cloning a repository differ from pulling from a repository?
+# Cloning a repository just creates a local copy of a remote repository, but pulling that repository pulls the latest updates to the repository so its used to update a repository. 
+# 7) Tell me a fun fact!
+# Capybaras are thw world's largent rodents. 
